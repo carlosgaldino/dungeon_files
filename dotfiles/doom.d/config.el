fdfdf1cbd5022bb11e47283ce1d77564c9101a65
Carlos Galdino <carloshsgaldino@gmail.com>
2024-04-27 17:53:20 +0100

    Wrap lines in markdown-mode

--- dotfiles/doom.d/config.el
+++ dotfiles/doom.d/config.el
@@ -84,6 +84,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(add-hook 'markdown-mode-hook 'turn-on-auto-fill)
 
 (use-package! auto-dark
   :config
