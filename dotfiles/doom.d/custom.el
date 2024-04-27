dfa2e87dc2ad0d010877db4d0d09fea9ab354b61
Carlos Galdino <carloshsgaldino@gmail.com>
2024-04-27 17:53:19 +0100

    Setup doom emacs

---
+++ dotfiles/doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c95813797eb70f520f9245b349ff087600e2bd211a681c7a5602d039c91a6428" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
