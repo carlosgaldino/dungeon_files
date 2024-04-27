58c4c51f6a3a1c01249dd6f59f070e9ed3d9a21b
Carlos Galdino <carloshsgaldino@gmail.com>
2024-04-27 17:53:21 +0100

    Configure PATH and other env vars

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,14 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -gx CDPATH $CDPATH . ~ $HOME/code
+    # set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
+    set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
+    set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
+    set -gx BUN_INSTALL "$HOME/.bun"
+
+    fish_add_path -m $HOME/.config/emacs/bin
+    fish_add_path -m $HOME/bin
     fish_add_path /opt/homebrew/opt/ruby/bin
+    fish_add_path -m /opt/homebrew/opt/llvm/bin
+    fish_add_path $BUN_INSTALL/bin
 
     alias g=git
     alias reload="source ~/.config/fish/config.fish"
 end
 
 starship init fish | source
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
