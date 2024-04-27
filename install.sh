ed5d83abee32c7eec3420b24be65b21ec2665a55
Carlos Galdino <carloshsgaldino@gmail.com>
2024-04-27 17:53:21 +0100

    Fix install

--- install.sh
+++ install.sh
@@ -10,17 +10,18 @@ link_files() {
   ln -s $1 $2
 }
 
+relink_file() {
+  if [ -e $2 ]; then
+    remove_file $2
+  fi
+
+  link_files $1 $2
+}
+
 for file in $HOME/.dungeon_files/dotfiles/*
 do
   home_file=$HOME/.$(basename $file)
-
-  if [ -e $home_file ]; then
-    remove_file $home_file
-  fi
-
-  link_files $file $home_file
-
-  echo ""
+  relink_file $file $home_file
 done
 
 rm -rf $HOME/.config
@@ -31,3 +32,11 @@ if [ -e $HOME/bin ]; then
 fi
 
 link_files $HOME/.dungeon_files/bin $HOME/bin
+
+if [ -d $HOME/.config ]; then
+  mkdir -p $HOME/.config/fish
+  mkdir -p $HOME/.config/ghostty
+fi
+
+relink_file $HOME/.dungeon_files/config/fish/config.fish $HOME/.config/fish/config.fish
+relink_file $HOME/.dungeon_files/config/ghostty/config $HOME/.config/ghostty/config
